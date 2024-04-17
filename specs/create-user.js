c704368a46a2734f3ff4fe9b1cd13053039761a0
cir94 <claire.isabella.ramos@gmail.com>
2024-04-17 17:56:06 -0400

    feat: added type:module to package, created mocharc config file and created first API test

---
+++ specs/create-user.js
@@ -0,0 +1,12 @@
+import { apiSend } from '../superagent-modules.js';
+import { expect } from 'chai';
+
+describe('ReqRes API - Creating a new user', function() {
+    it('should write information to the user object', async function() {
+        let req = await apiSend('https://reqres.in/api/users', {
+            name: 'morpheus',
+            job: 'leader',
+        });
+        expect(req.body.name).to.equal('morpheus');
+    })
+})
