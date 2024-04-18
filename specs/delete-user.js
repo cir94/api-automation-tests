e3a52ebefe26884684b9f4819e11cf6a04f60ca8
cir94 <claire.isabella.ramos@gmail.com>
2024-04-18 15:06:45 -0400

    style: changed tests to have same 'ReqRes API' description name and ran prettier on all files

--- specs/delete-user.js
+++ specs/delete-user.js
@@ -1,9 +1,9 @@
 import { expect } from 'chai';
 import { apiDelete } from '../superagent-modules.js';
 
-describe('ReqRes DELETE Test - Delete User', function() {
-    it('should delete the user in the given URL', async function() {
-        let req = await apiDelete('https://reqres.in/api/users/2');
-        expect(req.statusCode).to.equal(204);
-    });
+describe('ReqRes API - Delete User', function () {
+  it('should delete the user in the given URL', async function () {
+    let req = await apiDelete('https://reqres.in/api/users/2');
+    expect(req.statusCode).to.equal(204);
+  });
 });
--- specs/login-test.js
+++ specs/login-test.js
@@ -1,21 +1,21 @@
 import { expect } from 'chai';
 import { apiSend } from '../superagent-modules.js';
 
-describe("ReqRES - Login Test", function() {
-    let loginSuccess, loginFail;
-    before(async function() {
-        loginSuccess = await apiSend('https://reqres.in/api/login', {
-            email: 'eve.holt@reqres.in',
-            password: 'cityslicka',
-        });
-        loginFail = await apiSend('https://reqres.in/api/login', {
-            email: 'peter@klaven',
-        });
+describe('ReqRes API - Login Test', function () {
+  let loginSuccess, loginFail;
+  before(async function () {
+    loginSuccess = await apiSend('https://reqres.in/api/login', {
+      email: 'eve.holt@reqres.in',
+      password: 'cityslicka',
     });
+    loginFail = await apiSend('https://reqres.in/api/login', {
+      email: 'peter@klaven',
+    });
+  });
 
-    it('Should test for a successful login and a failed one', async function() {
-        expect(loginSuccess.statusCode).to.equal(200);
-        expect(loginSuccess.body.token).to.equal(`QpwL5tke4Pnpja7X4`);
-        expect(loginFail.response.body).to.include({ error: 'Missing password'});
-    });
+  it('Should test for a successful login and a failed one', async function () {
+    expect(loginSuccess.statusCode).to.equal(200);
+    expect(loginSuccess.body.token).to.equal(`QpwL5tke4Pnpja7X4`);
+    expect(loginFail.response.body).to.include({ error: 'Missing password' });
+  });
 });
