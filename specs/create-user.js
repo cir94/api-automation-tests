8875e12a5895c7d03bda9e1577481e8627d9a76f
cir94 <claire.isabella.ramos@gmail.com>
2024-04-19 15:59:10 -0400

    fix: changed the url in an uncommited .env file, requiring the pathname in create-user.js test to be changed to accomodate

--- specs/create-user.js
+++ specs/create-user.js
@@ -4,7 +4,7 @@ import { expect } from 'chai';
 
 describe('ReqRes API - Creating a new user', function () {
   it('should write information to the user object', async function () {
-    envs.apiTestingUrl.pathname += 'users';
+    envs.apiTestingUrl.pathname = '/api/users';
     let req = await apiSend(`${envs.apiTestingUrl}`, {
       name: 'morpheus',
       job: 'leader',
